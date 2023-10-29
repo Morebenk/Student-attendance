40a7d89cbb58c26af3db1471d1a42f6cdc15b043
Reda <morebenk@gmail.com>
2023-10-29 02:45:48 +0100

    uncommented some lines

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 
-// import { Amplify } from "aws-amplify";
-// import awsExports from "./aws-exports";
-// import "@aws-amplify/ui-vue/styles.css";
-// import "@aws-amplify/ui-vue";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import "@aws-amplify/ui-vue/styles.css";
+import "@aws-amplify/ui-vue";
 
-// Amplify.configure(awsExports);
+Amplify.configure(awsExports);
 
 createApp(App).use(router).mount("#app");
