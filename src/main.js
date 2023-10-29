2e86ce20b3f6aca99133591ea07b3d99869476d5
Reda <morebenk@gmail.com>
2023-10-29 02:27:30 +0100

    commented some lines

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-import "@aws-amplify/ui-vue/styles.css"
-import '@aws-amplify/ui-vue'
+// import { Amplify } from "aws-amplify";
+// import awsExports from "./aws-exports";
+// import "@aws-amplify/ui-vue/styles.css";
+// import "@aws-amplify/ui-vue";
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
